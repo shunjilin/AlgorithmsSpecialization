7476caaa93277f40d9f10547239d40775e9f38e7
Shunji Lin <lgxshunji@gmail.com>
2018-02-26 01:51:51 +0900

    add 02.Inversions

---
+++ 02.Inversions/include/MergeSort.hpp
@@ -0,0 +1,65 @@
+#ifndef MERGE_SORT_HPP
+#define MERGE_SORT_HPP
+
+#include <utility>
+#include <algorithm>
+#include <memory>
+
+// Trying out iterators, sorry if its too verbose
+namespace MergeSort {
+    
+    template <typename I, typename Compare>
+    void merge(const I first_begin, const I first_end,
+               const I second_begin, const I second_end,
+               I merge_space_begin,
+               int* inversion_count, const Compare comp) {
+        I lower = first_begin, upper = second_begin, merge = merge_space_begin;
+        while (lower != first_end && upper != second_end) {
+            if (comp(*lower, *upper)) {
+                (*merge++) = (*lower++);
+            } else {
+                (*merge++) = (*upper++);
+                if (inversion_count) *inversion_count += first_end - lower;
+            }
+        }
+        std::move(lower, first_end, merge);
+        std::move(upper, second_end, merge);
+    }
+
+    template<typename I, typename Compare>
+    void sortHelper(const I begin, const I end,
+                    const I merge_space_begin,
+                    int *inversion_count,
+                    const Compare comp) {
+        auto n = end - begin;
+        if (n <= 1) return;
+        auto midpoint = n / 2;
+        sortHelper(begin, begin + midpoint, merge_space_begin,
+                   inversion_count, comp);
+        sortHelper(begin + midpoint, end, merge_space_begin + midpoint,
+                   inversion_count, comp);
+        merge(begin, begin + midpoint, begin + midpoint, end,
+              merge_space_begin, inversion_count, comp);
+        std::move(merge_space_begin, merge_space_begin + n, begin);
+    }
+
+    template <typename I, typename Compare = std::less< typename I::value_type> >
+    void sort(const I begin, const I end,
+              int* inversion_count = nullptr,
+              const Compare comp = Compare()) {
+        std::vector<typename I::value_type> merge_space(end - begin); 
+        sortHelper(begin, end, std::begin(merge_space), inversion_count, comp);
+    }
+
+    template<typename I, typename Compare = std::less< typename I::value_type> >
+    int sortAndGetInversionCount(const I begin, const I end,
+                                 const Compare comp = Compare()) {
+        int inversion_count = 0;
+        sort(begin, end, &inversion_count, comp);
+        return inversion_count;
+    }
+
+    
+
+};
+#endif
---
+++ 02.Inversions/test/InversionsUnitTest.cpp
@@ -0,0 +1,38 @@
+#include <gmock/gmock.h>
+#include "MergeSort.hpp"
+#include <algorithm>
+
+using namespace testing;
+
+class InversionsInitialize: public Test {
+public:
+    std::vector<int> numbers = {4, 7, 3, 1, 6, 2, 8, 5};
+    std::vector<int> numbers_small = {3, 2, 1, 4};
+};
+
+TEST_F(InversionsInitialize, MergeTwoSortedListsCorrectly) {
+    std::vector<int> sorted_one = {1, 3, 5, 7};
+    std::vector<int> sorted_two = {2, 4, 6, 8};
+    std::vector<int> merged(sorted_one.size() + sorted_two.size());
+    MergeSort::merge(std::begin(sorted_one), std::end(sorted_one),
+                     std::begin(sorted_two), std::end(sorted_two),
+                     merged.begin(), nullptr, std::less<int>());
+    ASSERT_THAT(merged, ElementsAre(1, 2, 3, 4, 5, 6, 7, 8));
+}
+
+TEST_F(InversionsInitialize, SortListCorrectly) {
+    MergeSort::sort(numbers.begin(), numbers.end());
+    ASSERT_THAT(numbers, ElementsAre(1, 2, 3, 4, 5, 6, 7, 8));
+}
+
+TEST_F(InversionsInitialize, ReturnCorrectInverseCount) {
+    ASSERT_THAT(MergeSort::sortAndGetInversionCount(std::begin(numbers_small),
+                                                  std::end(numbers_small)),
+                3);
+}
+
+int main(int argc, char *argv[]) {
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
