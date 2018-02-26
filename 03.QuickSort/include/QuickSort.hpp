2f7140222927864e1e46c95dc42779c16ac68f76
Shunji Lin <lgxshunji@gmail.com>
2018-02-26 21:39:54 +0900

    add 03.QuickSort

---
+++ 03.QuickSort/include/QuickSort.hpp
@@ -0,0 +1,85 @@
+#ifndef QUICKSORT_HPP
+#define QUICKSORT_HPP
+#include <algorithm>
+
+namespace QuickSort {
+
+    template <typename I>
+    I getMid(const I begin, const I end) {
+        auto size = end - begin;
+        auto mid_offset = size % 2 == 0 ? size / 2 - 1 : size / 2;
+        return begin + mid_offset;
+    }
+
+    template <typename I, typename Compare>
+    I getMedianOfThree(const I one, const I two, const I three,
+                       Compare comp) {
+        auto min = std::max(std::min(*one, *two, comp),
+                            std::min(std::max(*one, *two, comp),
+                                     *three, comp), comp);
+        if (*one == min) return one;
+        if (*two == min) return two;
+        if (*three == min) return three;
+    }
+    
+    template <typename I, typename Compare = std::less<typename I::value_type> >
+    struct firstPivot {
+        I operator()(const I begin, const I end,
+                     const Compare comp = Compare()) const {
+            return (begin == end) ? end : begin;
+        }
+    };
+
+    template <typename I, typename Compare = std::less<typename I::value_type> >
+    struct lastPivot {
+        I operator()(const I begin, const I end,
+                     const Compare comp = Compare()) const {
+            return (begin == end) ? end : end-1;
+        }
+    };
+
+    template <typename I, typename Comp>
+    struct medianOfThreePivot {
+        I operator()(const I begin, const I end, const Comp comp) const {
+            if (begin == end) {
+                return end;
+            } else {
+                return getMedianOfThree(begin, getMid(begin, end), end, comp);
+            }
+        }
+    };
+
+    template<typename I, typename Compare>
+    I partition(const I begin, const I end, const I pivot,
+                   const Compare comp) {
+        if (end - begin <= 1) return begin;
+        using std::swap;
+        swap(*begin, *pivot);
+        I greater_begin = begin + 1, unpartitioned_begin = begin + 1;
+        while (unpartitioned_begin != end) {
+            if (comp(*unpartitioned_begin, *begin)) {
+                swap(*unpartitioned_begin, *greater_begin);
+                ++greater_begin;
+            }
+            ++unpartitioned_begin;
+        }
+        swap(*begin, *(greater_begin - 1));
+        return greater_begin - 1;
+    }
+
+    template <typename I, typename ChoosePivot,
+              typename Compare = std::less<typename I::value_type> >
+    void sort(const I begin, const I end,
+              const ChoosePivot choosePivot,
+              const Compare comp = Compare(),
+              unsigned long long *comparisons = nullptr) {
+        if (end - begin <= 1) return;
+        if (comparisons) *comparisons += end - begin - 1;
+        auto pivot = choosePivot(begin, end, comp);
+        pivot = partition(begin, end, pivot, comp); 
+        sort(begin, pivot, choosePivot, comp, comparisons);
+        sort(pivot + 1, end, choosePivot, comp, comparisons);
+    }
+};
+
+#endif
---
+++ 03.QuickSort/test/QuickSortUnitTest.cpp
@@ -0,0 +1,93 @@
+#include <gmock/gmock.h>
+#include "QuickSort.hpp"
+#include <algorithm>
+
+using namespace testing;
+
+class QuickSortInitialize: public Test {
+public:
+    std::vector<int> numbers = {4, 7, 3, 1, 6, 2, 8, 5};
+    std::vector<int> odd = {1, 2, 3};
+    std::vector<int> even = {1, 2, 3, 4};
+    std::vector<int> numbers_small = {2, 1};
+    std::vector<int> already_sorted = {1, 2, 3, 4, 5, 6, 7, 8};
+};
+
+TEST_F(QuickSortInitialize, FirstPivotReturnsCorrectly) {
+    ASSERT_THAT(*QuickSort::firstPivot<std::vector<int>::iterator>{}
+                (std::begin(numbers), std::end(numbers)), 4);
+}
+
+TEST_F(QuickSortInitialize, LastPivotReturnsCorrectly) {
+    ASSERT_THAT(*QuickSort::lastPivot<std::vector<int>::iterator>{}
+                (std::begin(numbers), std::end(numbers)), 5);
+}
+
+TEST_F(QuickSortInitialize, GetMidReturnsCorrectOnOdd) {
+    ASSERT_THAT(*QuickSort::getMid(std::begin(odd), std::end(odd)),
+                2);
+}
+
+TEST_F(QuickSortInitialize, GetMidReturnsCorrectOnEven) {
+    ASSERT_THAT(*QuickSort::getMid(std::begin(even), std::end(even)),
+                2);
+}
+
+TEST_F(QuickSortInitialize, GetMedianOfThreeReturnsCorrect) {
+    ASSERT_THAT(*QuickSort::getMedianOfThree(std::begin(odd),
+                                             std::begin(odd) + 1,
+                                             std::begin(odd) + 2,
+                                             std::less<int>()),
+                2);
+}
+
+TEST_F(QuickSortInitialize, PartitionCorrectly) {
+    QuickSort::partition(std::begin(numbers), std::end(numbers),
+                         std::begin(numbers) + 2, std::less<int>());
+    ASSERT_THAT(std::vector<int>(std::begin(numbers),
+                                 std::begin(numbers) + 3),
+                UnorderedElementsAre(1,2,3));
+}
+
+TEST_F(QuickSortInitialize, PartitionSmallCorrectly) {
+    QuickSort::partition(std::begin(numbers_small), std::end(numbers_small),
+                         std::begin(numbers_small), std::less<int>());
+    ASSERT_THAT(numbers_small, ElementsAre(1, 2));
+}
+
+TEST_F(QuickSortInitialize, QuickSorFirstPivotSortsCorrectly) {
+    QuickSort::sort(std::begin(numbers), std::end(numbers),
+                    QuickSort::firstPivot<std::vector<int>::iterator>(),
+                    std::less<int>());
+    ASSERT_THAT(numbers, ElementsAre(1, 2, 3, 4, 5, 6, 7, 8));
+}
+
+TEST_F(QuickSortInitialize, QuickSorLastPivotSortsCorrectly) {
+    QuickSort::sort(std::begin(numbers), std::end(numbers),
+                    QuickSort::lastPivot<std::vector<int>::iterator>(),
+                    std::less<int>());
+    ASSERT_THAT(numbers, ElementsAre(1, 2, 3, 4, 5, 6, 7, 8));
+}
+
+TEST_F(QuickSortInitialize, QuickSorMedianOfThreePivotSortsCorrectly) {
+    QuickSort::sort(std::begin(numbers), std::end(numbers),
+                    QuickSort::medianOfThreePivot<std::vector<int>::iterator,
+                    std::less<int> >(),
+                    std::less<int>());
+    ASSERT_THAT(numbers, ElementsAre(1, 2, 3, 4, 5, 6, 7, 8));
+}
+
+
+TEST_F(QuickSortInitialize, ComparisonCountAccurate) {
+    unsigned long long comparisons = 0;
+    QuickSort::sort(std::begin(already_sorted), std::end(already_sorted),
+                    QuickSort::firstPivot<std::vector<int>::iterator>(),
+                    std::less<int>(), &comparisons);
+    ASSERT_THAT(comparisons, 28);
+}
+
+
+int main(int argc, char *argv[]) {
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
